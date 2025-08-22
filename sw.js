fcc732d643cfef907d73314877fa90c8a5b85903
kidvision1000 <kidvision1000@gmail.com>
2025-08-21 23:25:34 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,3 @@
-// Simple cache-first service worker for offline usage
-const CACHE = 'kidvision-observations-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './app.jsx',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+const CACHE_NAME = "kidvision-observations-cache-v1.1";
+self.addEventListener('install', (e)=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./app.jsx','./manifest.json']))); });
+self.addEventListener('fetch', (e)=>{ e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request))); });
